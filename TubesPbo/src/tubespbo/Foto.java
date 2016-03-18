17ab5d07462a75ee32daf84bf6ff1beb33d1d306
edwinsatya <edwinsatyayudistira@gmail.com>
2016-03-19 00:10:34 +0700

    Tambahan Follow di Class Akun

--- TubesPbo/src/tubespbo/Foto.java
+++ TubesPbo/src/tubespbo/Foto.java
@@ -1,14 +1,44 @@
+package tubespbo;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tubespbo;
 
 /**
  *
- * @author kita
+ * @author user
  */
-public class Foto {
-    
+public class Foto extends Media {
+
+
+	public Foto(String nama, int ukuran, String jenis){
+		super(nama,ukuran,jenis);
+	}
+	
+	
+	public void setNama(String nama){
+		super.setNama(nama);
+	}
+	
+	public String getNama(){
+		return super.getNama();
+	}
+	
+	public void setUkuran(int ukuran){
+		super.setUkuran(ukuran);
+	}
+	
+	public int getUkuran(){
+		return super.getUkuran();
+	}
+	
+	public void setJenis(String jenis){
+		super.setJenis(jenis);
+	}
+	
+	public String getJenis(){
+		return super.getJenis();
+	}
 }
